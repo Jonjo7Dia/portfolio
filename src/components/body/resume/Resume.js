8536daf591bf134d1405ebe41d87083098585753
Jonathan <jonosphgames@gmail.com>
2022-05-24 19:14:24 -0300

    time to implement force scroll

--- src/components/body/resume/Resume.js
+++ src/components/body/resume/Resume.js
@@ -1,5 +1,15 @@
-function Resume(){
-    return <div>Resume</div>
+import classes from "./Resume.module.css";
+import CV from "../../files/CV-Image.png";
+import file from "../../files/cv.pdf";
+function Resume() {
+  return (
+    <div className={classes.resume}>
+      <a href={file} download={"Jonathan Hjelmstrom CV"}>
+        {" "}
+        <img src={CV} alt="" />
+      </a>
+    </div>
+  );
 }
 
 export default Resume;
