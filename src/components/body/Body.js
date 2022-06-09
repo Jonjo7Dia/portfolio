c20779af41dcec7dde83900e601c7819c6d93bce
Jonjo7Dia <jonosphgames@gmail.com>
2022-06-09 14:15:13 -0700

    fixing iphone

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -9,13 +9,25 @@ import Resume from "./resume/Resume";
 function Body(props) {
   var vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vh", vh + "px");
+  if (!!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform)) {
+    document.body.style.setProperty("max-height", "-webkit-fill-available");
+    document.documentElement.style.setProperty("max-height", "-webkit-fill-available");
+
+
+
+  }
+  else {
+    document.body.style.setProperty("height", "calc(var(--vh) * 100)");
+
+  }
   return (
     <div className={classes.body} id={"body"}>
       <BodyPart color={"#4495e5"} id={"about"} indicator={true}>
         <About
-         onView={(string) => {
-          props.onView(string);
-        }}></About>
+          onView={(string) => {
+            props.onView(string);
+          }}
+        ></About>
       </BodyPart>
       <BodyPart color={"#eb757f"} id={"skills"}>
         <MySkills
@@ -26,9 +38,10 @@ function Body(props) {
       </BodyPart>
       <BodyPart color={"#f5c544"} id={"exp"}>
         <Experience
-         onView={(string) => {
-          props.onView(string);
-        }}></Experience>
+          onView={(string) => {
+            props.onView(string);
+          }}
+        ></Experience>
       </BodyPart>
       <BodyPart color={"#4495e5"} id={"current"}>
         <CurrentProject
@@ -45,10 +58,11 @@ function Body(props) {
         ></Resume>
       </BodyPart>
       <BodyPart color={"#f5c544"} id={"contact"}>
-        <Contact 
-         onView={(string) => {
-          props.onView(string);
-        }}></Contact>
+        <Contact
+          onView={(string) => {
+            props.onView(string);
+          }}
+        ></Contact>
       </BodyPart>
     </div>
   );
