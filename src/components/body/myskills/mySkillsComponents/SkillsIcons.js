cf377285f39a96c2bf6b40fc0bc9ce2df776d5a4
Jonjo7Dia <jonosphgames@gmail.com>
2022-09-02 16:23:18 -0300

    skill icons updated with tool tip

--- src/components/body/myskills/mySkillsComponents/SkillsIcons.js
+++ src/components/body/myskills/mySkillsComponents/SkillsIcons.js
@@ -39,28 +39,43 @@ function SkillsIcons() {
     <div className={classes.skillsIcons} ref={myRef}>
       <div className={iconClasses[0]}>
         <img src={CSSLogo} alt="csslogo" />
+        <span className={classes.tooltiptext}>CSS</span>
+
       </div>
       <div className={iconClasses[1]}>
         <img src={HTMLLogo} alt="htmllogo" />
+        <span className={classes.tooltiptext}>HTML5</span>
+
       </div>
       <div className={iconClasses[2]}>
         <img src={JSLogo} alt="jslogo" />
+        <span className={classes.tooltiptext}>Typescript</span>
+
       </div>
       <div className={iconClasses[3]}>
         <img src={ReactLogo} alt="reactLogo" />
+        <span className={classes.tooltiptext}>ReactJS</span>
+
       </div>
       <div className={iconClasses[4]}>
         <img src={MongoLogo} alt="mongologo" />
+        <span className={classes.tooltiptext}>MongoDB</span>
+
       </div>
       <div className={iconClasses[5]}>
         <img src={ReduxLogo} alt="nodeLogo" />
+        <span className={classes.tooltiptext}>Redux</span>
+
       </div>
       <div className={iconClasses[6]}>
         <img src={NodeLogo} alt="nodeLogo" />
+        <span className={classes.tooltiptext}>Nodejs</span>
+
       </div>
      
-      <div className={iconClasses[7]}>
+      <div className={classes.tooltip, iconClasses[7]}>
         <img src={ExpressLogo} alt="expressLogo" />
+        <span className={classes.tooltiptext}>Express</span>
       </div>
     </div>
   );
